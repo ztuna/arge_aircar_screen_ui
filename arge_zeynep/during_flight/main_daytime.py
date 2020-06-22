e13a8cb45262649a8ac3cc45c8703b06b891324e
Zeynep Tuna <Zeynep.tuna@aircar.aero>
2020-06-22 09:31:27 +0000

    Updated main_daytime.py

--- arge_zeynep/during_flight/main_daytime.py
+++ arge_zeynep/during_flight/main_daytime.py
@@ -406,7 +406,7 @@ class GUI_MainWindow(QtWidgets.QMainWindow):
         
     def timerEvent(self):
         global time
-        time = time.addSecs(1)
+        time = time.addMSecs(200)
         return time
         
     def getTime(self):
